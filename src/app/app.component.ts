e3a3381ef90c84fa3269f38bb7cd8d04d91dc2b9
meerjavali <meerjavali98@gmail.com>
2024-06-25 00:04:26 +0530

    basic form creation in template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn';
+
+ // @ViewChild('f') form:NgForm;
+
+  onSubmit(f:NgForm){
+    console.log("the form is ", f);
+    console.log("form submitted");
+  }
 }
